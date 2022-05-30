5950dc5ab488b2c8b7187f889a0f65b702a6a125
zendybramantia <zendyoke@gmail.com>
2022-05-30 18:09:55 +0700

    profile widget

---
+++ lib/Profile/profile.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+  }
+}
